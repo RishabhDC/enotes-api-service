f5fa15174a4f282fc15fabdf6f6ad05e3fce0acc
Rishabhdev Chandrakar <chandrakarrd@gmail.com>
2024-11-14 09:51:22 +0530

    dto mapper implementation in category features

--- src/main/java/com/rdc/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/rdc/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,9 @@ package com.rdc.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import com.rdc.dto.CategoryDto;
+import com.rdc.dto.CategoryResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -17,15 +20,25 @@ public class CategoryServiceImpl implements CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepo;
 
+	@Autowired
+	private ModelMapper mapper;
+
 	@Override
-	public Boolean saveCategory(Category category) {
+	public Boolean saveCategory(CategoryDto categoryDto) {
+
+//		Category category = new Category();
+//		category.setName(categoryDto.getName());
+//		category.setDescription(categoryDto.getDescription());
+//		category.setIsActive(categoryDto.getIsActive());
+
+		Category category = mapper.map(categoryDto, Category.class);
 		
 		category.setIsDeleted(false);
 		category.setCreatedBy(1);
 		category.setCreatedOn(new Date());
-		
+
 		Category saveCategory = categoryRepo.save(category);
-		
+
 		if(ObjectUtils.isEmpty(saveCategory)) {
 			return false;
 		}
@@ -33,9 +46,21 @@ public class CategoryServiceImpl implements CategoryService {
 	}
 
 	@Override
-	public List<Category> getAllCategory() {
+	public List<CategoryDto> getAllCategory() {
 		List<Category> categories = categoryRepo.findAll();
-		return categories;
+
+		List<CategoryDto> categoryDtoList = categories.stream().map(cat -> mapper.map(cat, CategoryDto.class)).toList();
+
+		return categoryDtoList;
+	}
+
+	@Override
+	public List<CategoryResponse> getActiveCategory() {
+
+		List<Category> categories = categoryRepo.findByIsActiveTrueAndIsDeletedFalse();
+		List<CategoryResponse> categoryList = categories.stream().map(cat -> mapper.map(cat, CategoryResponse.class))
+				.toList();
+		return categoryList;
 	}
 
 }
